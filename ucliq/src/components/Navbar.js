c0e07f25c0224c8440906af60bd031ab05720ee8
Mahesh Kuldeep <mkuldeep313@gmail.com>
2022-09-29 20:54:07 +0530

    changes navbar

--- ucliq/src/components/Navbar.js
+++ ucliq/src/components/Navbar.js
@@ -1,18 +1,47 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import "../style/navbar.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/navbar.css";
 
 function Navbar() {
   return (
-    <div>
-    <ul className="navbar">
-        <li className='logo'>redbasil</li>
-        <li><Link to="/" className='home'>Home</Link></li>
-        <li><Link to="/service">What we do</Link></li>
-        <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-    </ul>
-    </div>
-  )
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            Redbasil
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <Link to="/" class="nav-link">
+                Home
+              </Link>
+
+              <Link to="/service" class="nav-link">
+                What we do
+              </Link>
+              <Link to="/about" class="nav-link">
+                About Us
+              </Link>
+              <Link to="/contact" class="nav-link">
+                Contact
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 export default Navbar;
