374d18c83ce9dbdab56b754091f60edb6b164831
Ajeet <ajeetbharti279@gmail.com>
2022-10-04 14:44:41 +0530

    how to register as buyer update

--- ucliq/src/components/SixthSection.js
+++ ucliq/src/components/SixthSection.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "../style/secondsection.css";
 import image3 from "../images/kyc1.jpg";
-//import image4 from "../images/download1.jpg";
-
 const SixthSection = () => {
   return (
     <>
@@ -10,37 +8,82 @@ const SixthSection = () => {
         <div className="fifthmain">
           <div className="sixth-two">
             <h1 className="fifthheading">How to register as a Buyer?</h1>
-            <div className="registerbuyer">
-              <img
-                src="https://res.cloudinary.com/antrix/image/upload/v1664818678/download_gif_cnuwyj.gif"
-                alt=""
-                style={{
-                  width: "30rem",
-                  padding: "0px",
-                  margin: "48px -21px 25px -1px",
-                  height: "22rem",
-                }}
-              />
-              <img
-                src="https://res.cloudinary.com/antrix/image/upload/v1664817925/WhatsApp_Image_2022-10-03_at_22.55.07_bwuo2k.jpg"
-                alt=""
-                style={{
-                  width: "21rem",
-                  padding: "0px",
-                  margin: "49px 3px 25px 85px",
-                  height: "22rem",
-                }}
-              />
-              <img
-                src={image3}
-                alt=""
-                style={{
-                  width: "30rem",
-                  padding: "0px",
-                  margin: "49px -104px 25px 69px",
-                  height: "22rem",
-                }}
-              />
+            <div className="col-md-4 registerbuyer">
+              <div className="registerInfo">
+                <div className="pic1">
+                  <img
+                    src="https://res.cloudinary.com/antrix/image/upload/v1664818678/download_gif_cnuwyj.gif"
+                    alt=""
+                    style={{
+                      width: "20rem",
+                      padding: "0px",
+                      margin: "48px 50px 9px -1px",
+                      height: "20rem",
+                    }}
+                  />
+                </div>
+                <div className="link1">
+                  <span >
+                    <a
+                      href="https://play.google.com/store/apps/details?id=com.ucliq_customer&gl=US"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Download the App
+                    </a>
+                  </span>
+                </div>
+              </div>
+              <div className="registerInfo">
+                <div className="pic2">
+                  <img
+                    src="https://res.cloudinary.com/antrix/image/upload/v1664817925/WhatsApp_Image_2022-10-03_at_22.55.07_bwuo2k.jpg"
+                    alt=""
+                    style={{
+                      width: "21rem",
+                      padding: "0px",
+                      margin: "49px 3px 9px 85px",
+                      height: "20rem",
+                    }}
+                  />
+                </div>
+                <div className="link2">
+                  <span>
+                    <a
+                      href="https://www.ucliq.in/login"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Signup
+                    </a>
+                  </span>
+                </div>
+              </div>
+              <div className="registerInfo">
+                <div className="pic3">
+                  <img
+                    src={image3}
+                    alt=""
+                    style={{
+                      width: "30rem",
+                      padding: "0px",
+                      margin: "49px -104px 9px 69px",
+                      height: "20rem",
+                    }}
+                  />
+                </div>
+                <div className="link3">
+                  <span>
+                    <a
+                      href="https://www.ucliq.in/kyc"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                     Start KYC
+                    </a>
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
