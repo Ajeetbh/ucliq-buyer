3e0beb2e759faccc1ad2de9c86037a62a29c5ecc
Mahesh Kuldeep <mkuldeep313@gmail.com>
2022-10-11 22:34:00 +0530

    css update

--- ucliq/src/components/SixthSection.js
+++ ucliq/src/components/SixthSection.js
@@ -4,31 +4,42 @@ import "../style/sixsection.css";
 const SixthSection = () => {
   return (
     <div className="container">
-        <h1 style={{color:"#af1212",fontSize:"3rem"}}>How to register as a Buyer?</h1>
-        <div className="flex-container">
-            <div className="divcol2" data-aos="zoom-in">
-                <h5>Download Ucliq <br/> Buyer App</h5>
-                <a href="https://play.google.com/store/apps/details?id=com.ucliq_customer&gl=US" target="_blank"
-                rel="noreferrer">
-                    <i className="fa fa-android" aria-hidden="true" style={{fontSize:"48px",color:"black"}}></i>    
-                </a>
-            </div>
-            <div className="divcol3" data-aos="zoom-in">
-                <i className="fa fa-mobile" style={{fontSize:"48px",color:"#f3da35"}}></i>
-                <h5>Verify mobile number</h5>
-            </div>
-            <div className="divcol4" data-aos="zoom-in">
-                <h5>Complete KYC</h5>
-                <i className="fa fa-home" style={{fontSize:"48px"}}></i>
-            </div>
+      <h1 style={{ color: "#af1212", fontSize: "3rem" }}>
+        How to register as a Buyer?
+      </h1>
+      <div className="flex-container">
+        <div className="divcol2" data-aos="zoom-in">
+          <h5>
+            Download Ucliq <br /> Buyer App
+          </h5>
+          <a
+            href="https://play.google.com/store/apps/details?id=com.ucliq_customer&gl=US"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i
+              className="fa fa-android"
+              aria-hidden="true"
+              style={{ fontSize: "48px", color: "black" }}
+            ></i>
+          </a>
         </div>
-    </div> 
+        <div className="divcol3" data-aos="zoom-in">
+          <i
+            className="fa fa-mobile"
+            style={{ fontSize: "48px", color: "#f3da35" }}
+          ></i>
+          <h5 style={{ margin: "-10px 1px" }}>Verify mobile number</h5>
+        </div>
+        <div className="divcol4" data-aos="zoom-in">
+          <h5>Complete KYC</h5>
+          <i className="fa fa-home" style={{ fontSize: "48px" }}></i>
+        </div>
+      </div>
+    </div>
   );
 };
 export default SixthSection;
-
-
-
 
 // import React from "react";
 // import "../style/secondsection.css";
@@ -126,12 +137,12 @@ export default SixthSection;
 //                 <div className="link3">
 //                   <span>
 //                    <a
-                    //   href="https://www.ucliq.in/kyc"
-                    //   target="_blank"
-                    //   rel="noreferrer"
-                    // >
-                    //   Start KYC
-                    // </a>
+//   href="https://www.ucliq.in/kyc"
+//   target="_blank"
+//   rel="noreferrer"
+// >
+//   Start KYC
+// </a>
 //                   </span>
 //                 </div>
 //               </div>
